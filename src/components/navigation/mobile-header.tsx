7c1054ad0ebcb4e08bd8610b6019c2ab614b0aa8
Marti Gatchev <mgatchev@gmail.com>
2025-10-18 20:34:31 -0700

    changed speed of animation for nav mobile menu

--- src/components/navigation/mobile-header.tsx
+++ src/components/navigation/mobile-header.tsx
@@ -54,7 +54,7 @@ export function MobileHeader({ borders = false, links }: MobileHeaderProps) {
       transition: {
         type: "tween",
         ease: "easeOut",
-        duration: 0.9,
+        duration: 0.6,
         when: "beforeChildren", // parent enters first, then children
       },
     },
